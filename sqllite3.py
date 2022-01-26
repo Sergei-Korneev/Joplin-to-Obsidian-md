e0d4e2671cac327efe7bb90e0fe98dca1e94d5fc
s.m.korneev@yandex.ru <sergei@pop-os.localdomain>
2022-01-26 18:11:33 +0500

    initial commit

---
+++ sqllite3.py
@@ -0,0 +1,48 @@
+import sqlite3, re, os, shutil
+
+path='./JoplinProfileExport'
+expath=path+'/Obsidian/'
+respath=expath+'/resources/'
+
+def __find(filename, search_path):
+   result = []
+
+   for root, dir, files in os.walk(search_path):
+       for f in files:
+           if  filename in f:
+             result.append(os.path.join(root, f))
+   return result
+
+
+
+
+try:
+    con = sqlite3.connect(path+'/joplin.sqlite')
+    cur = con.cursor()
+except OSError as error:
+    print(error)
+
+
+try: 
+    os.mkdir(expath)
+    os.mkdir(respath)
+except OSError as error: 
+    print(error)  
+
+
+for row in cur.execute('SELECT title,body FROM notes'):
+    
+    print("Exporting: "+ row[0])
+    filetmp=row[1]
+    for line in re.findall("!\[.*\]\(.*\)", filetmp):
+      
+      t=line.split('(')[1::2][0].split(')')[0].replace(":/","")
+      shutil.copyfile(__find(t, path+'/resources')[0],respath+__find(t, path+'/resources')[0].split("/")[-1])
+      t='![['+__find(t, path+'/resources')[0].split("/")[-1]+']]'
+      #print(t)
+      filetmp=re.sub(line.replace("!","\!").replace("[","\[").replace("]","\]").replace(")","\)").replace("(","\("),t,filetmp)
+  
+    note_file=open(expath + row[0] + ".md",'w')           
+    note_file.write(filetmp) 
+    note_file.close()
+
