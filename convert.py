3222086b23c057255e18781131f1feb09106ae45
Sergei Korneev <s.m.korneev@yandex.ru>
2022-09-04 21:10:13 +0500

    Windows OS fix

--- convert.py
+++ convert.py
@@ -1,23 +1,30 @@
-import sqlite3, re, sys, os, shutil
+import sqlite3, sys, os, shutil, re, codecs
 
 def cmdhelp():
     print("\nUse python3 convert.py [path to exported joplin profile]\n")
     sys.exit(1)
+    
+def  chkpath(p):
+    if not (os.path.exists(p)):
+       print("\nThe path " + str(p) + " does not exist.")
+       cmdhelp()
 
 def main():
 
-    if (len(sys.argv)==1) or  not os.path.exists(sys.argv[1]):    
-            print("\nThe path  does not exist.")
+    if (len(sys.argv)==1):            
             cmdhelp()
+    chkpath(sys.argv[1]) 
 
     path=sys.argv[1]
     pathres=os.path.join(path,'resources')
+    dbpath=os.path.join(path,'joplin.sqlite')
     expath=os.path.join(path,'Obsidian')
     respath=os.path.join(expath,'resources')
+    
+    chkpath(pathres)
+    chkpath(dbpath)
 
-
-
-    def __find(filename, search_path):
+    def findfiles(filename, search_path):
        result = []
 
        for root, dir, files in os.walk(search_path):
@@ -48,12 +55,12 @@ def main():
         filetmp=row[1]
         for line in re.findall("!\[.*\]\(.*\)", filetmp):
           
-          t=line.split('(')[1::2][0].split(')')[0].replace(":/","")
-          shutil.copyfile(__find(t, pathres)[0],os.path.join(respath,os.path.basename(__find(t, pathres)[0])))
-          t='![['+__find(t, pathres)[0].split("/")[-1]+']]'
+          t=line.split("(")[1::2][0].split(")")[0].replace(":/","")
+          shutil.copyfile(findfiles(t, pathres)[0],os.path.join(respath,os.path.basename(findfiles(t, pathres)[0])))
+          t='![['+os.path.basename(findfiles(t, pathres)[0])+']]'
           filetmp=re.sub(line.replace("!","\!").replace("[","\[").replace("]","\]").replace(")","\)").replace("(","\("),t,filetmp)
       
-        note_file=open(os.path.join(expath , row[0] + ".md"),'w')           
+        note_file=codecs.open(os.path.join(expath , row[0] + ".md"),"w","utf-8")           
         note_file.write(filetmp) 
         note_file.close()
     con.close()
