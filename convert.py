e9ac40bcc2f7633029faa69f24f4ed99a2048c7d
Sergei Korneev <s.m.korneev@yandex.ru>
2023-01-08 15:06:49 +0500

    Add metadata and fixes

--- convert.py
+++ convert.py
@@ -1,4 +1,7 @@
 import sqlite3, sys, os, shutil, re, codecs
+from  get import Download
+import datetime
+
 
 def cmdhelp():
     print("\nUse python3 convert.py [path to exported joplin profile]\n")
@@ -19,23 +22,35 @@ def makepath(p):
 
 # Replace Windows/Linux filesystem reserved symbols
 def repl_forb(string):
-  forbidden_char={'<': '(less than)',
-                 '>': '(greater than)',
-                 ':': '(colon)',
-                 '"': '(double quote)',
-                 '/': '(forward slash)',
-                 '\\': '(backslash)',
-                 '|': '(vertical bar or pipe)',
-                 '?': '(question mark)',
-                 '*': '(asterisk)'
+  forbidden_char={'<': '\u2770',
+                 '>': '\u2771',
+                 ':': '\u003a',
+                 '"': '\u275d',
+                 '/': '\u27cb',
+                 '\\': '\u27cd',
+                 '|': '\u2758',
+                 '#': '\u27da',
+                 '?': '\u2754',
+                 '*': '\u2731'
                  }
 
 
   for   rep in forbidden_char:
                  string=string.replace(rep,forbidden_char[rep])
+  if len(string) > 250:
+      string=string[0:240]+"~"
+
   return string
 
 
+def findfiles(filename, search_path):
+       result = []
+
+       for root, dir, files in os.walk(search_path):
+           for f in files:
+               if  filename in f:
+                 result.append(os.path.join(root, f))
+       return result
 
 def main():
 
@@ -51,16 +66,6 @@ def main():
     
     chkpath(dbpath)
     chkpath(pathres)
-
-    def findfiles(filename, search_path):
-       result = []
-
-       for root, dir, files in os.walk(search_path):
-           for f in files:
-               if  filename in f:
-                 result.append(os.path.join(root, f))
-       return result
-
 
     try:
         con = sqlite3.connect(os.path.join(path,'joplin.sqlite'))
@@ -78,18 +83,29 @@ def main():
         makepath(os.path.join(expath,folders[row[0]]))
 
     print("Exporting to :" + expath + "\n\nNotebook\tNote\n")
-    for row in cur.execute('SELECT title,body,parent_id FROM notes'):
+    meta=""
+    for row in cur.execute('SELECT title,body,parent_id,id,source_url,created_time,source FROM notes'):
         print(folders[row[2]] + "\t" +  row[0])
         filetmp=row[1]
         for line in re.findall("!\[.*\]\(.*\)", filetmp):
-          
-          t=line.split("(")[1::2][0].split(")")[0].replace(":/","")
-          shutil.copyfile(findfiles(t, pathres)[0],os.path.join(respath,os.path.basename(findfiles(t, pathres)[0])))
-          t='![['+os.path.basename(findfiles(t, pathres)[0])+']]'
-          filetmp=re.sub(line.replace("!","\!").replace("[","\[").replace("]","\]").replace(")","\)").replace("(","\("),t,filetmp)
-      
-        note_file=codecs.open(os.path.join(expath, folders[row[2]], repl_forb(row[0]) + ".md"), "w", "utf-8")           
-        note_file.write(filetmp) 
+          t=re.sub('^\:\/', '', line.split("(")[1::2][0].split(")")[0])
+          if t[0:4]!="http":
+              #print("Downloading an attachement:\r\n"+t+"\r\n")
+             # t=Download(t,True,pathres+'/')
+              t=os.path.basename(findfiles(t, pathres)[0])
+              try:
+                  shutil.copyfile(os.path.join(pathres,t),os.path.join(respath,t))
+                  t='![['+t+']]'
+                  filetmp=re.sub(line.replace("!","\!").replace("[","\[").replace("]","\]").replace(")","\)").replace("(","\("),t,filetmp)
+              except OSError as error:
+                print(error)
+
+          date_t= datetime.datetime.fromtimestamp(row[5] // 1000).strftime("%m/%d/%Y, %H:%M:%S")
+          meta="\r\n***\r\nNote id: "+row[3]+"\r\nUrl: "+row[4]+"\r\nCreated time (db, local): "+date_t+"\r\nSource app: "+row[6]
+        note_file=codecs.open(os.path.join(expath, folders[row[2]], repl_forb(row[0]) + ".md"), "w", "utf-8")   
+
+        note_file.write(filetmp)
+        note_file.write(meta)
         note_file.close()
     con.close()
 
