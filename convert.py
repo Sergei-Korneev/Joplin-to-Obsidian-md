63738871ad43246b6d84ea674dd0b5eb017b8211
Sergei Korneev <s.m.korneev@yandex.ru>
2023-01-13 13:46:28 +0500

    Add Desktop support

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import sqlite3, sys, os, shutil, re, codecs
 import datetime
 import markdownify
+import functools
 
 def cmdhelp():
     print("\nUse python3 convert.py [path to exported joplin profile]\n")
@@ -13,7 +14,7 @@ def  chkpath(p):
 
 def makepath(p):
     try: 
-        os.mkdir(p)
+        os.makedirs(p, exist_ok=True)
     except OSError as error:
         if error.errno != 17:
           print(error)  
@@ -51,6 +52,17 @@ def findfiles(filename, search_path):
                  result.append(os.path.join(root, f))
        return result
 
+def getDirs(parent,cur):
+        path = []
+        while  parent:
+
+            for  res in  cur.execute('SELECT title,parent_id FROM folders WHERE id = \"' + parent+'\"'):
+
+                title=repl_forb(res[0])
+                parent=res[1]
+                path.append(title) 
+        return functools.reduce(os.path.join,list(reversed(path)))
+
 def main():
 
     if (len(sys.argv)==1) or (len(sys.argv)>2):
@@ -59,46 +71,51 @@ def main():
 
     path=sys.argv[1]
     pathres=os.path.join(path,'resources')
-    dbpath=os.path.join(path,'joplin.sqlite')
+    dbpathm=os.path.join(path,'joplin.sqlite')
+    dbpathd=os.path.join(path,'database.sqlite')
     expath=os.path.join(path,'Obsidian')
     respath=os.path.join(expath,'resources')
-    
-    chkpath(dbpath)
+    dbpath=""
+    if  (os.path.exists(dbpathm)):
+        dbpath=dbpathm
+    elif  (os.path.exists(dbpathd)):
+        dbpath=dbpathd
+    else:
+        print("No db file [mobile or desktop] found in "+path+"\r\n")
+        exit(1)
+
     chkpath(pathres)
 
     try:
-        con = sqlite3.connect(os.path.join(path,'joplin.sqlite'))
+        con = sqlite3.connect(dbpath)
         cur = con.cursor()
+        cur2 = con.cursor()
     except OSError as error:
         print(error)
         sys.exit(1)
 
     makepath(expath)
     makepath(respath)
-
-    folders = {}
-    for row in cur.execute('SELECT id,title FROM folders'):
-        folders[row[0]] = repl_forb(row[1])
-        makepath(os.path.join(expath,folders[row[0]]))
-
-    print("Exporting to :" + expath + "\n\nNotebook\tNote\n")
+    
+    print("Exporting to: " + expath )
     meta=""
     fol=""
-    for row in cur.execute('SELECT title,body,parent_id,id,source_url,created_time,source FROM notes'):
-        if row[2] in folders:
-                fol=folders[row[2]]
-        else:
-            fol=""
-        print(fol + "\t" +  row[0])
+
+    cur1 = cur.execute('SELECT title,body,parent_id,id,source_url,created_time,source FROM notes')
+    for row in cur1:
+    
+        fol = os.path.join(expath,getDirs(row[2],cur2))
+        makepath(fol)
+
+        print ("----\r\nFolder: "+fol+"\r\n")
+        print("Note: \'" +  row[0]+"\'" )
         filetmp=markdownify.markdownify(row[1], heading_style="ATX").replace("](:/","](").replace("\\*","*").replace("\\_","_")
         for line in re.findall("\!\[.*\]\(.*\)", filetmp):
           jop_res=re.sub('^\:\/', '', line.split("(")[1::2][0].split(")")[0]).split(" ")[0]
           #print("att: "+jop_res)
           if jop_res[0:4]!="http":
-              #print("Downloading an attachement:\r\n"+t+"\r\n")
-             # t=Download(t,True,pathres+'/')
               try:
-                  f_list=findfiles(jop_res, pathres) 
+                  f_list=findfiles(jop_res, pathres)
                   if (len(f_list) > 0):
                       obs_res=os.path.basename(f_list[0])
                     #  print("file: " +obs_res)
@@ -106,7 +123,7 @@ def main():
                       filetmp=re.sub("\("+jop_res+"\)", "("+obs_res+")", filetmp) 
                       filetmp=re.sub("\("+jop_res+" ", "("+obs_res+" ", filetmp) 
                   else:
-                      print("Warning: Attachement "+jop_res+" in note \'"+row[0]+"\' not found in "+pathres)
+                      print("Warning: Attachement "+jop_res+", note: \'"+row[0]+"\' not found in: "+pathres)
 
 
               except OSError as error:
@@ -114,12 +131,13 @@ def main():
 
           date_t= datetime.datetime.fromtimestamp(row[5] // 1000).strftime("%m/%d/%Y, %H:%M:%S")
           meta="\r\n***\r\nNote id: "+row[3]+"\r\nUrl: "+row[4]+"\r\nCreated time (db, local): "+date_t+"\r\nSource app: "+row[6]
-        note_file=codecs.open(os.path.join(expath, fol, repl_forb(row[0]) + ".md"), "w", "utf-8")   
+        note_file=codecs.open(os.path.join(fol, repl_forb(row[0]) + ".md"), "w", "utf-8")   
 
         note_file.write(filetmp)
         note_file.write(meta)
         note_file.close()
     con.close()
+    print("\r\n-----Finished!------")
 
 
 
